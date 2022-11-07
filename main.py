3f4e8c419200ef7f7494767b7ca9465a935237dc
djelinska <dominikajelinska82@gmail.com>
2022-11-07 19:59:40 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import pytest
+
+
+def func(x):
+    return x + 1
---
+++ testing_func.py
@@ -0,0 +1,16 @@
+from main import func
+
+
+def test_method1():
+    assert func(3) == 4
+
+
+def test_method2():
+    a = 5
+    b = 10
+    assert 2*a == b
+
+
+def test_method3():
+    txt = "hello"
+    assert "h" in txt
